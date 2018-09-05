44949178e5761d0823f9ef7453f9cc843f1adfaa
Antônio Camargo <antoniop.camargo@gmail.com>
2018-09-05 12:47:52 -0300

    Improve the wording of the 'output_features' argument

--- mopal.py
+++ mopal.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     parser.add_argument('fasta_file', help='FASTA file containing complete sequences of protein-coding transcripts.')
     parser.add_argument('classification_model_file', help='FASTA file containing complete sequences of noncoding transcripts.')
     parser.add_argument('hex_table_file', help='Hexamer frequency table file.')
-    parser.add_argument('--output_features', choices=['yes', 'no'], default='no', help='Output computed features and the classification probability.')
+    parser.add_argument('--output_features', choices=['yes', 'no'], default='no', help='Output computed features and the coding probability.')
     parser.add_argument('--output_file', help='Save output to a file. If not set, the output will be printed on the screen.')
     parser.add_argument('--hmmer_cpu', default=1, help='Number of parallel CPU to use for multithreads in HMMER.')
     if len(sys.argv) < 2:
