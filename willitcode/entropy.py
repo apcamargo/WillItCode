7c29e8681f4f8a7967e7af2de4adf671cc3ef0d7
Antônio Pedro Camargo <apcamargo@users.noreply.github.com>
2019-01-11 15:27:36 -0200

    Don't compute entropy for ORFs with a single codon

--- willitcode/entropy.py
+++ willitcode/entropy.py
@@ -10,12 +10,12 @@ def get_codon_entropy(record):
     sequence_str = str(record.seq.upper())
     orf_counter = Counter(codon[0] for codon in get_codons(sequence_str))
     total_codon_count = sum(orf_counter.values())
-    if total_codon_count <= 1:
+    orf_frequencies = []
+    for codon in orf_counter.keys():
+        orf_frequencies.append(orf_counter[codon]/total_codon_count)
+    if len(orf_frequencies) <= 1:
         return 1
     else:
-        orf_frequencies = []
-        for codon in orf_counter.keys():
-            orf_frequencies.append(orf_counter[codon]/total_codon_count)
         entropy = ss.entropy(orf_frequencies)
         norm_entropy = entropy/np.log(len(orf_frequencies))
         return norm_entropy
